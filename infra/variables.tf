32f5d9d6e2474b49837365cc3318cad1b5c06e83
Ben Clark <benjamin.r.clark@gmail.com>
2016-06-12 13:35:57 +0100

    Initial commit

---
+++ infra/variables.tf
@@ -0,0 +1,43 @@
+variable "environment" {
+   description = "Name of this environment"
+   default = "production"
+}
+
+variable "aws_access_key" {
+    description = "Access key for AWS"
+}
+
+variable "aws_secret_key" {
+    description = "Secret key for AWS"
+}
+
+variable "aws_region" {
+    description = "Region where we will operate."
+    default = "eu-west-1"
+}
+
+variable "aws_availability_zones" {
+    type = "map"
+    description = "Map of region to availability zone"
+    default {
+        "us-east-1"      = ""
+        "us-west-2"      = ""
+        "us-west-1"      = ""
+        "eu-west-1"      = "eu-west-1a,eu-west-1b,eu-west-1c"
+        "eu-central-1"   = ""
+        "ap-southeast-1" = ""
+        "ap-northeast-1" = ""
+        "ap-southeast-2" = ""
+        "ap-northeast-2" = ""
+        "sa-east-1"      = ""
+    }
+}
+
+variable "aws_availability_zone_count" {
+  description = "The number of availability zones we require"
+  default     = "3"
+}
+
+variable "ssh_public_key" {
+    description = "Contents of an SSH public key to grant access to created instances"
+}
